261b009d788281adab10ba85bee10f7dd14e03e0
Ricarnicero <ricardo.torres@mccollect.com.mx>
2021-01-21 13:43:29 -0600

    first commit

---
+++ 01 Neon/App_Data/bleNeon.cs
@@ -0,0 +1,116 @@
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Neon.App_Data
+{
+    class bleNeon
+    {
+        private IDevice connectedDevice;
+        private Guid ServiceUID;
+        private Guid CharacteristcUID;
+        public bleNeon(string ServiceUID, string CharacteristcUID)
+        {
+            connectedDevice = null;
+            this.ServiceUID = new Guid(ServiceUID);
+            this.CharacteristcUID = new Guid(CharacteristcUID);
+        }
+
+        public async void ConnectToMyDevice()
+        {
+            try
+            {
+                var adapter = CrossBluetoothLE.Current.Adapter;
+                if (connectedDevice == null)
+                {
+
+                    var devices = adapter.GetSystemConnectedOrPairedDevices();
+                    foreach (IDevice device in devices)
+                    {
+                        if (device.Name == "ESP32test")
+                        {
+                            connectedDevice = device;
+                            if (connectedDevice.State == Plugin.BLE.Abstractions.DeviceState.Disconnected)
+                            {
+                                await adapter.ConnectToDeviceAsync(connectedDevice, new Plugin.BLE.Abstractions.ConnectParameters(autoConnect: true, forceBleTransport: true));
+                                adapter.DeviceDisconnected += (s, a) =>
+                                {
+                                    connectedDevice = null;
+                                };
+                                break;
+                            }
+
+                        }
+                    }
+                }
+                else if (connectedDevice.State == Plugin.BLE.Abstractions.DeviceState.Disconnected)
+                {
+                    await adapter.ConnectToDeviceAsync(connectedDevice, new Plugin.BLE.Abstractions.ConnectParameters(autoConnect: true, forceBleTransport: true));
+                    adapter.DeviceDisconnected += (s, a) =>
+                    {
+                        connectedDevice = null;
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                var e = ex.Message;
+            }
+        }
+
+        public async void Write(string data)
+        {
+            try
+            {
+                var service = await connectedDevice.GetServiceAsync(ServiceUID);
+                var characteristic = await service.GetCharacteristicAsync(CharacteristcUID);
+                if (characteristic != null)
+                {
+                    if (characteristic.CanWrite)
+                    {
+                        byte[] rv = new byte[1 + data.Length];
+                        rv[0] = 200;
+
+                        var dataBytes = Encoding.ASCII.GetBytes(data);
+
+                        dataBytes.CopyTo(rv, 1);
+
+                        await characteristic.WriteAsync(rv);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("-----------------------------------------------------");
+                Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public async void Write(byte[] data)
+        {
+            try
+            {
+                var service = await connectedDevice.GetServiceAsync(ServiceUID);
+                var characteristic = await service.GetCharacteristicAsync(CharacteristcUID);
+                if (characteristic != null)
+                {
+                    if (characteristic.CanWrite)
+                    {
+                        await characteristic.WriteAsync(data);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("-----------------------------------------------------");
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
---
+++ 01 Neon/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using _01_Neon.App_Data;
+
+namespace _01_Neon
+{
+    public partial class MainPage : ContentPage
+    {
+        bleNeon ble = null;
+        public MainPage()
+        {
+            InitializeComponent();
+            ble = new bleNeon("6E400001-B5A3-F393-E0A9-E50E24DCCA9E", "6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+        }
+
+        //await Task.Delay(300);
+        private void btnEncendidoNormal_Clicked(object sender, System.EventArgs e)
+        {
+            ble.Write("002");
+        }
+
+        private void btnEncendidoApagar_Clicked(object sender, System.EventArgs e)
+        {
+            ble.Write("003");
+        }
+        private void btnEncendidoChido_Clicked(object sender, System.EventArgs e)
+        {
+            ble.Write("004");
+        }
+
+        private void btnSegurosPoner_Clicked(object sender, System.EventArgs e)
+        {
+            ble.Write("005");
+        }
+
+        private void btnSegurosQuitar_Clicked(object sender, System.EventArgs e)
+        {
+            ble.Write("006");
+        }
+
+        private void btnConectar_Clicked(object sender, System.EventArgs e)
+        {
+            ble.ConnectToMyDevice();
+        }
+
+        private void btnCajuela_Clicked(object sender, System.EventArgs e)
+        {
+            ble.Write("007");
+        }
+    }
+}
